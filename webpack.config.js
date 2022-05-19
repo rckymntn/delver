bdb85be77b038043f32f72beda68446713bd7ce5
Logan Raffkind <logan.raffkind@gmail.com>
2022-05-18 17:18:02 -0700

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.ts',
+    module: {
+        rules:[{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development'
+};
