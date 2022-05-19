5701406284e962c84a00e20720dd4157a1976b1f
Logan Raffkind <logan.raffkind@gmail.com>
2022-05-19 13:19:50 -0700

    Run on body load

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,5 @@
-import { Display } from "rot-js/lib/index";
+import { GameManager } from "./GameManager";
 
-/*
- *  Temporary sanity check
- */
-function sanityCheck() {
-    let options = {
-        width: 75,
-        height: 30,
-        fontSize: 16,
-        spacing: 1.0
-    };
-    let display = new Display(options);
-    document.body.appendChild(display.getContainer());
-    
-    for (let x = 0; x < options.width; ++x) {
-        for (let y = 0; y < options.height; ++y) {
-            if (!x || !y || x + 1 == options.width || y + 1 == options.height) {
-                display.draw(x, y, "#", "lightgray", "black");
-            } else {
-                display.draw(x, y, ".", "lightgray", "black");
-            }
-        }
-    }
-    
-    display.draw(options.width >> 1, options.height >> 1, "@", "goldenrod", "black");
+document.body.onload = () => {
+    const gameManager = new GameManager();
 }
-
-sanityCheck();
