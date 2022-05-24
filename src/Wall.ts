9c1eec7309f86e7db85f9c7360c90dd38270883f
Logan Raffkind <logan.raffkind@gmail.com>
2022-05-24 11:54:04 -0700

    Created wall prop

---
+++ src/Wall.ts
@@ -0,0 +1,16 @@
+import { Glyph } from "./Glyph";
+import { Position } from "./Position";
+import { Prop, PropType } from "./Prop"
+
+export class Wall implements Prop {
+
+    glyph: Glyph;
+    type: PropType;
+    isPassable: boolean;
+
+    constructor(public position: Position) {
+        this.position = position;
+        this.glyph = new Glyph("#", "lightgray", "black");
+        this.type = PropType.Wall;
+    }
+}
