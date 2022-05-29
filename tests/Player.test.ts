128d48602c8d47a9824bb0e8c998daf7d8ea648e
Logan Raffkind <logan.raffkind@gmail.com>
2022-05-29 16:08:10 -0700

    Test Player class

---
+++ tests/Player.test.ts
@@ -0,0 +1,29 @@
+import { KEYS } from "rot-js";
+import { ActorType } from "../src/Actor";
+import { Player } from "../src/Player";
+import { Position } from "../src/Position";
+
+test("glyph", () => {
+    let player: Player = new Player(new Position(0, 0));
+    expect(player.glyph.char).toEqual("@");
+    expect(player.glyph.fgColor).toEqual("goldenrod");
+    expect(player.glyph.bgColor).toEqual("black");
+});
+
+test("type", () => {
+    let player: Player = new Player(new Position(0, 0));
+    expect(player.type).toEqual(ActorType.Player);
+});
+
+test("position", () => {
+    let player: Player = new Player(new Position(0, 0));
+    expect(player.position).toEqual(new Position(0, 0));
+});
+
+test("keybinds", () => {
+    let player: Player = new Player(new Position(0, 0));
+    expect(player.keyBinds[KEYS.VK_A]).toEqual(0);
+    expect(player.keyBinds[KEYS.VK_D]).toEqual(1);
+    expect(player.keyBinds[KEYS.VK_W]).toEqual(2);
+    expect(player.keyBinds[KEYS.VK_S]).toEqual(3);
+});
