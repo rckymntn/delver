a1d02b4884c1ee19b025c4783003631ce812182b
Logan Raffkind <logan.raffkind@gmail.com>
2022-05-30 12:21:13 -0700

    Initial work

--- src/MapManager.ts
+++ src/MapManager.ts
@@ -1,3 +1,31 @@
+import { Position } from "./Position";
+
 export class MapManager {
     
+    constructor() {
+
+    }
+
+    generateMap() {
+
+    }
+
+    positionToKey(position: Position): string {
+        return `(${position.x}, ${position.y})`;
+    }
+
+    keyToPosition(key: string): Position {
+        let splitKey: string[] = key.split(",");
+        let x: number = Number(splitKey[0].replace("(", ""));
+        let y: number = Number(splitKey[1].replace(")", ""));
+        return new Position(x, y);
+    }
+
+    public resolveNewPosition(position: Position, dxdy: number[]): Position {
+        return new Position(position.x + dxdy[0], position.y + dxdy[1]);
+    }
+
+    isPassable(): boolean {
+        return false;
+    }
 }
