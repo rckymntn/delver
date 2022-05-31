3a60ca1c7fb7520424c03779c8902b294c14b444
Logan Raffkind <logan.raffkind@gmail.com>
2022-05-31 00:55:10 -0700

    Test Position class

---
+++ tests/Position.test.ts
@@ -0,0 +1,23 @@
+import { Position } from "../src/Position";
+
+test("construction of a new position", () => {
+    let position = new Position(0, 0);
+    expect(position.x).toEqual(0);
+    expect(position.y).toEqual(0);
+});
+
+test("equal positions", () => {
+    let position1: Position = new Position(0, 0);
+    let position2: Position = new Position(0, 0);
+    let expected: boolean = true;
+    let actual: boolean = position1.equals(position2);
+    expect(expected).toEqual(actual);
+});
+
+test("not equal positions", () => {
+    let position1: Position = new Position(0, 0);
+    let position2: Position = new Position(1, 1);
+    let expected: boolean = false;
+    let actual: boolean = position1.equals(position2);
+    expect(expected).toEqual(actual);
+});
