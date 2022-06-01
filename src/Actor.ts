ad94d5efd7f9572f5fe0a526dc4fdfefd18ef7ae
Logan Raffkind <logan.raffkind@gmail.com>
2022-06-01 00:25:27 -0700

    Update action method signature

--- src/Actor.ts
+++ src/Actor.ts
@@ -1,7 +1,8 @@
 import { Entity } from "./Entity"
+import { MapManager } from "./MapManager";
 
 export interface Actor extends Entity {
-    action(): Promise<any>;
+    action(mapManager: MapManager): Promise<any>;
 }
 
 export const enum ActorType {
