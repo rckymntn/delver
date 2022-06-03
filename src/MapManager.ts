8a4b134ef1546b83970e1638d48f8f7553b4a7e7
Logan Raffkind <logan.raffkind@gmail.com>
2022-06-03 14:45:52 -0700

    Reorder methods

--- src/MapManager.ts
+++ src/MapManager.ts
@@ -1,7 +1,7 @@
-import { Map } from "rot-js";
+import { Map, RNG } from "rot-js";
 import { Floor } from "./Floor";
 import { Position } from "./Position";
-import { Tile } from "./Tile";
+import { Tile, TileType } from "./Tile";
 import { Wall } from "./Wall";
 
 export class MapManager {
@@ -10,6 +10,37 @@ export class MapManager {
 
     constructor() {
         this.map = {};
+    }
+
+    getPassable(position: Position): boolean {
+        return this.map[this.positionToKey(position)].getPassable();
+    }
+
+    setPassable(posiiton: Position, boolean: boolean): void {
+        this.map[this.positionToKey(posiiton)].setPassable(boolean);
+    }
+
+    getOccupied(position: Position): boolean {
+        return this.map[this.positionToKey(position)].getOccupied();
+    }
+
+    setOccupied(posiiton: Position, boolean: boolean): void {
+        this.map[this.positionToKey(posiiton)].setOccupied(boolean);
+    }
+
+    positionToKey(position: Position): string {
+        return `(${position.x}, ${position.y})`;
+    }
+
+    keyToPosition(key: string): Position {
+        let splitKey: string[] = key.split(",");
+        let x: number = Number(splitKey[0].replace("(", ""));
+        let y: number = Number(splitKey[1].replace(")", ""));
+        return new Position(x, y);
+    }
+
+    resolveNewPosition(position: Position, dxdy: number[]): Position {
+        return new Position(position.x + dxdy[0], position.y + dxdy[1]);
     }
 
     diggerMap(x: number, y: number): void {
@@ -67,36 +98,5 @@ export class MapManager {
             return;
         }
         this.map[this.positionToKey(position)] = new Floor(position);
-    }
-
-    getPassable(position: Position): boolean {
-        return this.map[this.positionToKey(position)].getPassable();
-    }
-
-    setPassable(posiiton: Position, boolean: boolean): void {
-        this.map[this.positionToKey(posiiton)].setPassable(boolean);
-    }
-
-    getOccupied(position: Position): boolean {
-        return this.map[this.positionToKey(position)].getOccupied();
-    }
-
-    setOccupied(posiiton: Position, boolean: boolean): void {
-        this.map[this.positionToKey(posiiton)].setOccupied(boolean);
-    }
-
-    positionToKey(position: Position): string {
-        return `(${position.x}, ${position.y})`;
-    }
-
-    keyToPosition(key: string): Position {
-        let splitKey: string[] = key.split(",");
-        let x: number = Number(splitKey[0].replace("(", ""));
-        let y: number = Number(splitKey[1].replace(")", ""));
-        return new Position(x, y);
-    }
-
-    resolveNewPosition(position: Position, dxdy: number[]): Position {
-        return new Position(position.x + dxdy[0], position.y + dxdy[1]);
     }
 }
