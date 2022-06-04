3bf6a6a0699bdb1e2a72fdad3540facd501e960d
Logan Raffkind <logan.raffkind@gmail.com>
2022-06-04 10:13:06 -0700

    Get and set position

--- src/Goblin.ts
+++ src/Goblin.ts
@@ -1,21 +1,30 @@
 import { Actor, ActorType } from "./Actor";
 import { Glyph } from "./Glyph";
+import { MapManager } from "./MapManager";
 import { Position } from "./Position";
 
 export class Goblin implements Actor {
     
     glyph: Glyph;
     type: ActorType;
-    isPassable: boolean;
+    passable: boolean;
     
     constructor(public position: Position) {
         this.position = position;
         this.glyph = new Glyph("G", "green", "black");
         this.type = ActorType.Goblin;
-        this.isPassable = false;
+        this.passable = false;
     }
 
-    action(): Promise<any> {
+    getPosition(): Position {
+        return this.position;
+    }
+
+    setPosition(posiiton: Position): void {
+        this.position = posiiton;
+    }
+
+    action(mapManager: MapManager): Promise<any> {
         return;
     }
 }
