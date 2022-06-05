d49e84309bd3f86d94406d52416ffb1de7de27e8
Logan Raffkind <logan.raffkind@gmail.com>
2022-06-05 10:14:13 -0700

    Added access modifiers

--- src/MapManager.ts
+++ src/MapManager.ts
@@ -12,38 +12,34 @@ export class MapManager {
         this.map = {};
     }
 
-    getPassable(position: Position): boolean {
+    public getPassable(position: Position): boolean {
         return this.map[this.positionToKey(position)].getPassable();
     }
 
-    setPassable(posiiton: Position, boolean: boolean): void {
-        this.map[this.positionToKey(posiiton)].setPassable(boolean);
+    public setPassable(position: Position, boolean: boolean): void {
+        this.map[this.positionToKey(position)].setPassable(boolean);
     }
 
-    getOccupied(position: Position): boolean {
+    public getOccupied(position: Position): boolean {
         return this.map[this.positionToKey(position)].getOccupied();
     }
 
-    setOccupied(posiiton: Position, boolean: boolean): void {
-        this.map[this.positionToKey(posiiton)].setOccupied(boolean);
+    public setOccupied(position: Position, boolean: boolean): void {
+        this.map[this.positionToKey(position)].setOccupied(boolean);
     }
 
-    positionToKey(position: Position): string {
+    public positionToKey(position: Position): string {
         return `(${position.x}, ${position.y})`;
     }
 
-    keyToPosition(key: string): Position {
+    public keyToPosition(key: string): Position {
         let splitKey: string[] = key.split(",");
         let x: number = Number(splitKey[0].replace("(", ""));
         let y: number = Number(splitKey[1].replace(")", ""));
         return new Position(x, y);
     }
 
-    resolveNewPosition(position: Position, dxdy: number[]): Position {
-        return new Position(position.x + dxdy[0], position.y + dxdy[1]);
-    }
-
-    diggerMap(x: number, y: number): void {
+    public diggerMap(x: number, y: number): void {
         let digger = new Map.Digger(x, y);
         digger.create(this.diggerCallback.bind(this));
     }
@@ -57,7 +53,7 @@ export class MapManager {
         this.map[this.positionToKey(position)] = new Floor(position);
     }
 
-    arenaMap(x: number, y: number) {
+    public arenaMap(x: number, y: number) {
         let arena = new Map.Arena(x, y);
         arena.create(this.arenaCallback.bind(this));
     }
@@ -71,7 +67,7 @@ export class MapManager {
         this.map[this.positionToKey(position)] = new Floor(position);
     }
 
-    mazeMap(x: number, y: number) {
+    public mazeMap(x: number, y: number) {
         let maze = new Map.DividedMaze(x, y);
         maze.create(this.mazeCallback.bind(this));
     }
@@ -85,7 +81,7 @@ export class MapManager {
         this.map[this.positionToKey(position)] = new Floor(position);
     }
 
-    cellularMap(x: number, y: number, randomness: number = 0.5) {
+    public cellularMap(x: number, y: number, randomness: number = 0.5) {
         let cellular = new Map.Cellular(x, y);
         cellular.randomize(randomness);
         cellular.create(this.cellularCallback.bind(this));
