90a8c608827ab9ce17c8d9e49aad3a529464ef3e
Logan Raffkind <logan.raffkind@gmail.com>
2022-06-12 10:55:40 -0700

    Accessors

--- src/Glyph.ts
+++ src/Glyph.ts
@@ -4,4 +4,28 @@ export class Glyph {
         this.fgColor = fgColor;
         this.bgColor = bgColor;
     }
+
+    public getChar(): string {
+        return this.char;
+    }
+
+    public setChar(char: string): void {
+        this.char = char;
+    }
+
+    public getForegroundColor(): string {
+        return this.fgColor;
+    }
+
+    public setForegroundColor(fgColor: string): void {
+        this.fgColor = fgColor;
+    }
+
+    public getBackgroundColor(): string {
+        return this.bgColor;
+    }
+
+    public setBackgroundColor(bgColor: string): void {
+        this.bgColor = bgColor;
+    }
 }
