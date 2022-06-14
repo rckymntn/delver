ad62ab93471af57ba318e62905d9ce14b4ebf6a5
Logan Raffkind <logan.raffkind@gmail.com>
2022-06-14 12:31:24 -0700

    Some tests for Floor

---
+++ tests/Floor.test.ts
@@ -0,0 +1,34 @@
+import { Floor } from "../src/Floor";
+import { Position } from "../src/Position";
+import { TileType } from "../src/Tile";
+
+test("glyph", () => {
+    let floor: Floor = new Floor(new Position(0, 0));
+    expect(floor.glyph.getChar()).toEqual(".");
+    expect(floor.glyph.getForegroundColor()).toEqual("lightgray");
+    expect(floor.glyph.getBackgroundColor()).toEqual("black");
+});
+
+test("position", () => {
+    let floor: Floor = new Floor(new Position(0, 0));
+    expect(floor.getPosition()).toEqual(new Position(0, 0));
+    floor.setPosition(new Position(1, 1));
+    expect(floor.getPosition()).toEqual(new Position(1, 1));
+});
+
+test("occupied", () => {
+    let floor: Floor = new Floor(new Position(0, 0));
+    expect(floor.getOccupied()).toEqual(false);
+    floor.setOccupied(true);
+    expect(floor.getOccupied()).toEqual(true);
+});
+
+test("passable", () => {
+    let floor: Floor = new Floor(new Position(0, 0));
+    expect(floor.getPassable()).toEqual(true);
+});
+
+test("tiletype", () => {
+    let floor: Floor = new Floor(new Position(0, 0));
+    expect(floor.getType()).toEqual(TileType.Floor);
+});
