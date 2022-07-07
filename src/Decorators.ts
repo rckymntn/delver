1d8ebd07753971779687425c9713941d92b16a78
Logan Raffkind <logan.raffkind@gmail.com>
2022-07-07 11:24:04 -0700

    Indentation

--- src/Decorators.ts
+++ src/Decorators.ts
@@ -3,10 +3,10 @@
  *  Starts a timer, runs a method, stops the timer, and outputs time elapsed to console
  */
 export const timer = (target: Object, propertyKey: string, propertyDescriptor: PropertyDescriptor) => {
-        const originalMethod = propertyDescriptor.value;
-        propertyDescriptor.value = function(...args) {
-            console.time(`${propertyKey}`);
-            originalMethod.apply(this, args);
-            console.timeEnd(`${propertyKey}`);
-        }
-    }
+    const originalMethod = propertyDescriptor.value;
+    propertyDescriptor.value = function(...args) {
+        console.time(`${propertyKey}`);
+        originalMethod.apply(this, args);
+        console.timeEnd(`${propertyKey}`);
+    };
+};
