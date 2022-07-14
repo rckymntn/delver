2554871c889d425c0506181765e105c065c238e4
Logan Raffkind <logan.raffkind@gmail.com>
2022-07-14 11:05:57 -0700

    Added tests for leftPad

--- tests/TextUtil.test.ts
+++ tests/TextUtil.test.ts
@@ -1,6 +1,6 @@
 import { TextUtil } from "../src/TextUtil";
 
-test("center pad, even string and even width", () => {
+test("centerPad, even string and even width", () => {
     let string: string = "abcdef";
     let width: number = 10;
     let expected: string = "\0\0abcdef\0\0";
@@ -8,7 +8,7 @@ test("center pad, even string and even width", () => {
     expect(expected).toEqual(actual);
 });
 
-test("center pad, odd string and even width", () => {
+test("centerPad, odd string and even width", () => {
     let string: string = "abcdefg";
     let width: number = 10;
     let expected: string = "\0\0abcdefg\0";
@@ -16,7 +16,7 @@ test("center pad, odd string and even width", () => {
     expect(expected).toEqual(actual);
 });
 
-test("center pad, even string and odd width", () => {
+test("centerPad, even string and odd width", () => {
     let string: string = "abcdef";
     let width: number = 9;
     let expected: string = "\0\0abcdef\0";
@@ -24,10 +24,74 @@ test("center pad, even string and odd width", () => {
     expect(expected).toEqual(actual);
 });
 
-test("center pad, odd string and odd width", () => {
+test("centerPad, odd string and odd width", () => {
     let string: string = "abcdefg";
     let width: number = 9;
     let expected: string = "\0abcdefg\0";
     let actual: string = TextUtil.centerPad(string, width);
+    expect(expected).toEqual(actual);
+});
+
+test("centerPad, empty string and even width", () => {
+    let string: string = "";
+    let width: number = 10;
+    let expected: string = "\0\0\0\0\0\0\0\0\0\0";
+    let actual: string = TextUtil.centerPad(string, width);
+    expect(expected).toEqual(actual);
+});
+
+test("centerPad, empty string and odd width", () => {
+    let string: string = "";
+    let width: number = 9;
+    let expected: string = "\0\0\0\0\0\0\0\0\0";
+    let actual: string = TextUtil.centerPad(string, width);
+    expect(expected).toEqual(actual);
+});
+
+test("leftPad, even string and even width", () => {
+    let string: string = "abcdef";
+    let width: number = 10;
+    let expected: string = "\0\0\0\0abcdef";
+    let actual: string = TextUtil.leftPad(string, width);
+    expect(expected).toEqual(actual);
+});
+
+test("leftPad, odd string and even width", () => {
+    let string: string = "abcdefg";
+    let width: number = 10;
+    let expected: string = "\0\0\0abcdefg";
+    let actual: string = TextUtil.leftPad(string, width);
+    expect(expected).toEqual(actual);
+});
+
+test("leftPad, even string and odd width", () => {
+    let string: string = "abcdef";
+    let width: number = 9;
+    let expected: string = "\0\0\0abcdef";
+    let actual: string = TextUtil.leftPad(string, width);
+    expect(expected).toEqual(actual);
+});
+
+test("leftPad, odd string and odd width", () => {
+    let string: string = "abcdefg";
+    let width: number = 9;
+    let expected: string = "\0\0abcdefg";
+    let actual: string = TextUtil.leftPad(string, width);
+    expect(expected).toEqual(actual);
+});
+
+test("leftPad, empty string and even width", () => {
+    let string: string = "";
+    let width: number = 10;
+    let expected: string = "\0\0\0\0\0\0\0\0\0\0";
+    let actual: string = TextUtil.leftPad(string, width);
+    expect(expected).toEqual(actual);
+});
+
+test("leftPad, empty string and odd width", () => {
+    let string: string = "";
+    let width: number = 9;
+    let expected: string = "\0\0\0\0\0\0\0\0\0";
+    let actual: string = TextUtil.leftPad(string, width);
     expect(expected).toEqual(actual);
 });
