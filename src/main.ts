e4f2408e3db2a7e45b1efdaf17a0e3fb34fa5511
Logan Raffkind <logan.raffkind@gmail.com>
2022-08-29 21:50:21 -0700

    Experimenting with a different way to do dynamic display sizing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { GameManager } from "./GameManager";
 
+const canvas = document.querySelector("canvas");
+
 document.body.onload = () => {
     const gameManager = new GameManager();
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
