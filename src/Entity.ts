39db56e69fc8e991ff395f249e53966f2180a780
Logan Raffkind <logan.raffkind@gmail.com>
2022-09-11 10:20:20 -0700

    Class header comment

--- src/Entity.ts
+++ src/Entity.ts
@@ -1,6 +1,9 @@
 import { Glyph } from "./Glyph";
 import { Position } from "./Position";
 
+/*
+ *  An entity is anything rendered on the map. Tiles, props, actors, etc. 
+ */
 export interface Entity {
     position: Position;
     glyph: Glyph;
