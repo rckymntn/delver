d796753331475d6312b66dc4077579784c580208
Logan Raffkind <logan.raffkind@gmail.com>
2022-09-12 18:48:22 -0700

    Update Actor.ts

--- src/Actor.ts
+++ src/Actor.ts
@@ -1,6 +1,9 @@
 import { Entity } from "./Entity"
 import { MapManager } from "./MapManager";
 
+/*
+ *  An actor is a type of entity that has some dynamic element to it (action method)
+ */ 
 export interface Actor extends Entity {
     action(mapManager: MapManager);
 }
