67044edfb05096ab88f5ea2ddcd0d712f8566c80
Logan Raffkind <logan.raffkind@gmail.com>
2022-09-16 17:24:42 -0700

    Update Prop.ts

--- src/Prop.ts
+++ src/Prop.ts
@@ -1,5 +1,8 @@
 import { Entity } from "./Entity";
 
+/*
+ *  A prop is an entity that is not an actor or tile. This is for things like decorations or items on the map
+ */
 export interface Prop extends Entity {
 
 }
